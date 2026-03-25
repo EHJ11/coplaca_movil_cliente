e2eadd608f167a955de169d2bc07b9e6c84e4e7f
educole3335 <usr3335@salesianos-lacuesta.net>
2026-03-25 10:27:41 +0000

    chore(index.ts): actualizar components/common/index.ts
    Cambio: +13/-0 lineas en components/common/index.ts.
    
    Motivo: mantener trazabilidad por archivo y facilitar revision o rollback puntual.

---
+++ components/common/index.ts
@@ -0,0 +1,13 @@
+// Components
+export { default as AppHeader } from './AppHeader';
+export { default as ProductCard } from './ProductCard';
+export { default as WalletCard } from './WalletCard';
+export { default as CartItem } from './CartItem';
+export { default as OrderSummary } from './OrderSummary';
+export { default as MessageAlert } from './MessageAlert';
+export { default as FilterPanel } from './FilterPanel';
+export { default as OffersCarousel } from './OffersCarousel';
+export { default as SearchBar } from './SearchBar';
+export { default as FormInput } from './FormInput';
+export { default as PrimaryButton } from './PrimaryButton';
+export { default as LoadingSpinner } from './LoadingSpinner';
