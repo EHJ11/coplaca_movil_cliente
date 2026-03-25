b8686c2c078b67434a5805aef4b7f1e958b207c3
educole3335 <usr3335@salesianos-lacuesta.net>
2026-03-25 10:30:01 +0000

    chore(index.ts): actualizar src/components/common/index.ts
    Cambio: +9/-0 lineas en src/components/common/index.ts.
    
    Motivo: mantener trazabilidad por archivo y facilitar revision o rollback puntual.

---
+++ src/components/common/index.ts
@@ -0,0 +1,9 @@
+/**
+ * Index for exporting common components
+ */
+
+export { Button } from "./Button";
+export { Input } from "./Input";
+export { ThemedText } from "./ThemedText";
+export { ThemedView } from "./ThemedView";
+
